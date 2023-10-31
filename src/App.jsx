dab06d4103bd61aa833dc02e29ac066e2b4c2b3a
Yury <matatov1989@gmail.com>
2023-10-31 13:55:51 +0200

    set style to buttons & inputs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8 flex">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
       <NewProject />
     </main>
